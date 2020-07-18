d5ba5071ec29a5add1e57f263e5ae3a5213e8e41
Ryuya Ikeda <rikeda71@gmail.com>
2020-07-18 15:29:00 +0900

    p10 target=es2015 module=amd

--- Practice-Typescript/section1/test.js
+++ Practice-Typescript/section1/test.js
@@ -1,7 +1,9 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.test = void 0;
-function test() {
-    return 'test';
-}
-exports.test = test;
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.test = void 0;
+    function test() {
+        return 'test';
+    }
+    exports.test = test;
+});
