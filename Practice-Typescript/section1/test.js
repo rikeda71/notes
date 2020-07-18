b67e29133ee5405fa89106f45c6df13f3c1eb98a
Ryuya Ikeda <rikeda71@gmail.com>
2020-07-18 15:31:05 +0900

    p11 error caused

--- Practice-Typescript/section1/test.js
+++ Practice-Typescript/section1/test.js
@@ -1,3 +1,7 @@
-export function test() {
-    return 'test';
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.test = void 0;
+function test(arg) {
+    return arg;
 }
+exports.test = test;
