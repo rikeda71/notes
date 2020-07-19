b5336ef8d4bae8480cfc8b026a4fbdbbf5006dd5
Ryuya Ikeda <rikeda71@gmail.com>
2020-07-19 17:17:05 +0900

    p57-59 type inference for Array and Tuple

--- Practice-Typescript/section3/type-inference.ts
+++ Practice-Typescript/section3/type-inference.ts
@@ -37,3 +37,56 @@ const zeros = {
   zeroC, // 0
   zeroD, // 0
 };
+
+// Arrayの型推論
+const a1 = [true, false];
+const a2 = [0, 1, '2'];
+const a3 = [false, 1, '2'];
+
+/*
+const a1 : boolean[]
+const a2 : (string | number)[]
+const a3 : (string | number | boolean)[]
+*/
+
+// assersion to array
+const a4 = [0 as 0, 1 as 1];
+a4.push(1);
+a4.push(2); // error
+
+const zero: 0 = 0;
+const one: 1 = 1;
+const a5 = [zero, one]; // const a1: (0 | 1)[]
+a5.push(1);
+a5.push(2); // error
+
+// Tupleの型推論
+// 記法だけではArrayと差別化できないため、as [type, ...]を後ろにつける
+const t1 = [false] as [boolean];
+const t2 = [false, 1] as [boolean, number];
+const t3 = [false, 1, '2'] as [boolean, number, string];
+const v3_0 = t3[0];
+const v3_1 = t3[1];
+const v3_2 = t3[2];
+const v3_3 = t3[3]; // error
+
+// 要素の追加の場合、Tupleに含まれている型のUnion Typesになる
+// booleanのみ追加できる
+t1.push(false);
+t1.push(true);
+t1.push(1); // error
+// boolean | numberを追加できる
+t2.push(false);
+t2.push(1);
+t2.push('2'); // error
+
+// Array型推論
+let list = ['this', 'is', 'a', 'test'];
+list.push('!');
+console.log(list);
+
+list = list.map((item) => item.toUpperCase()); // item string
+console.log(list);
+
+let message = list.reduce((prev, current) => `${prev} ${current}`);
+console.log(message);
