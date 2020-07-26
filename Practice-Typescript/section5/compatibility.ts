bb6dccdc0883f0fdc88634c572d6f5157a6f4dc7
Ryuya Ikeda <rikeda71@gmail.com>
2020-07-26 17:19:24 +0900

    p99, 100 compatibility for any and unknown

--- Practice-Typescript/section5/compatibility.ts
+++ Practice-Typescript/section5/compatibility.ts
@@ -10,3 +10,18 @@ let n1: 0 = 0;
 let n2: number = n1; // no error
 let n3: number = 0;
 let n4: 0 = n3; // error
+
+// anyの互換性
+// anyはどんな型にも宣言、代入ができる
+let a1: any = false; // let a1: any
+let a2: string = a1; // let a2: string
+let a3 = a1 as number; // let a3: number
+
+// unknown型の互換性
+// unknown = どんな型の値も受け入れることができるTopType
+// anyとは違い、型が決定するまで、別の型に代入できない
+let un1: unknown = 'test';
+let un2: string = un1; // compile error
+let un3: number = un1 as number; // 誤った型宣言
+// 互換性のないアサーション
+const s3 = 0 as string; // compile error
