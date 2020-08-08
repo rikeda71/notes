6e63b2498589b202ae0bf9ea9aaeefbecdad6972
Ryuya Ikeda <rikeda71@gmail.com>
2020-08-08 16:58:08 +0900

    p116-118 generics for function

--- Practice-Typescript/section6/generics.ts
+++ Practice-Typescript/section6/generics.ts
@@ -25,3 +25,28 @@ interface EBox<T extends string | number> {
 const ebox0: EBox<string> = { value: 'test' };
 const ebox1: EBox<number> = { value: 0 };
 const ebox2: EBox<boolean> = { value: false }; // error
+
+// 関数のGenerics
+
+function boxed<T>(props: T) {
+  return { value: props };
+}
+// 関数のGenericsは暗黙的に解決（推論）される
+const fbox0 = boxed('test');
+const fbox1 = boxed(0);
+const fbox2 = boxed(false);
+const fbox3 = boxed(null);
+
+/// アサーションによる明示的な型の付与
+/// Nullable型などを直接適応したい場合に使う
+const fbox4 = boxed(false as booelan | null);
+const fbox5 = boxed<string | null>(null);
+/// 関数の代入は以下のようにもできる
+const boxed2 = <T>(props: T) => ({ value: props });
+
+/// extendsによる制約
+function eboxed<T extends string>(props: T) {
+  return { value: props };
+}
+const efbox1 = eboxed(0); // error
+const efbox2 = eboxed('test');
