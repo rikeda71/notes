5f73d836708ebd2b6b220d4a3212b9e82eab3767
Ryuya Ikeda <rikeda71@gmail.com>
2020-08-14 17:04:58 +0900

    p151-154 refactor data type

--- Practice-Typescript/section7/table/tr.tsx
+++ Practice-Typescript/section7/table/tr.tsx
@@ -3,7 +3,7 @@ import { Row } from '../data';
 
 const Component: React.FC<Row> = (props) => (
   <tr>
-    <th>{props.generation}</th>
+    <th>{props.age}</th>
     {props.answers.map((answer, i) => (
       // answer: number
       <td key={i}>{`${answer * 100}`}</td>
