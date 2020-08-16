f33a1726a772f5b5ebe381c33784096d5dcf2d00
Ryuya Ikeda <rikeda71@gmail.com>
2020-08-16 15:28:23 +0900

    p271-273 introduce styled-component with SSR

--- Practice-Typescript/section10/components/index.tsx
+++ Practice-Typescript/section10/components/index.tsx
@@ -1,8 +1,20 @@
 // Components
 // 自動的にルーティングを適応させたくないページを構成するコンポーネントをおく
 import React from 'react';
+import Next, { NextPageContext } from 'next';
 import { NextComponentType } from 'next';
+import styled from 'styled-components';
 
-const Page: NextComponentType = () => <div>Welcome to next.js</div>;
+type Props = {
+  className?: string;
+};
 
-export default Page;
+const Component: NextComponentType<NextPageContext, {}, Props> = (props) => (
+  <div className={props.className}>Welcome to next.js</div>
+);
+
+const StyledComponent = styled(Component)`
+  color: #f00;
+`;
+
+export default StyledComponent;
--- Practice-Typescript/section10/pages/_document.tsx
+++ Practice-Typescript/section10/pages/_document.tsx
@@ -4,11 +4,21 @@
 
 import React from 'react';
 import Document, { DocumentContext } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 import DefaultLayout from '../layouts/index';
 
 export default class extends Document {
   // ctxはサーバーサイドのみで受け取ることができるreq/resがふくまれている
   static async getInitialProps(ctx: DocumentContext) {
+    // styled-componentsのSSRの指定
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App: any) => (props: any) =>
+          sheet.collectStyles(<App {...props} />),
+      });
+
     const initialProps = await Document.getInitialProps(ctx);
     return { ...initialProps };
   }
