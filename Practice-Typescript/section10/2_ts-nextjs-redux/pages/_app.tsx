d6b353a2ac8828136ff5646cee5e51e8f666b6fc
Ryuya Ikeda <rikeda71@gmail.com>
2020-08-16 16:13:07 +0900

    p274-284 use redux with nextjs

---
+++ Practice-Typescript/section10/2_ts-nextjs-redux/pages/_app.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react'
+import { Provider } from 'react-redux'
+import App, { AppContext } from 'next/app'
+import withRedux from 'next-redux-wrapper'
+import { initStore, ReduxStore } from '../store'
+// ______________________________________________________
+//
+
+type Props = {
+  store: ReduxStore
+}
+
+export default withRedux(initStore)(
+  class extends App<Props> {
+    static async getInitialProps({ Component, ctx }: AppContext) {
+      let pageProps = {}
+      if (Component.getInitialProps) {
+        pageProps = await Component.getInitialProps(ctx)
+      }
+      // StoreState の推論が適用されている
+      // const state = ctx.store.getState()
+      return { pageProps }
+    }
+    render() {
+      const { Component, pageProps, store } = this.props
+      return (
+        <Provider store={store}>
+          <Component {...pageProps} />
+        </Provider>
+      )
+    }
+  }
+)
