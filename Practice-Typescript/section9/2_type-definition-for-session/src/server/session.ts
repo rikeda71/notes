6719bbc9bbef20569c0a7d96ffd5e48e97f3b3b2
Ryuya Ikeda <rikeda71@gmail.com>
2020-08-16 10:47:37 +0900

    fix server with redis

--- Practice-Typescript/section9/2_type-definition-for-session/src/server/session.ts
+++ Practice-Typescript/section9/2_type-definition-for-session/src/server/session.ts
@@ -1,14 +1,18 @@
 import Express from 'express'
 import session from 'express-session'
+import redis from 'redis'
 import connectRedis from 'connect-redis'
 import { REDIS_HOST, REDIS_PORT } from '../constants'
 
 export default (app: Express.Application) => {
   const RedisStore = connectRedis(session)
+  const redisClient = redis.createClient()
   const option = {
     store: new RedisStore({
-      host: REDIS_HOST,
-      port: REDIS_PORT,
+      client: redisClient,
+      // 以下の方法で繋げなかったので、修正
+      // host: REDIS_HOST,
+      // port: REDIS_PORT,
     }),
     secret: 'keyboard cat',
     resave: false,
