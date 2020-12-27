e686b9667b38a35574b088a7a34fdf188f684ee4
Ryuya Ikeda <ryuyiked@yahoo-corp.jp>
2020-12-27 16:41:33 +0900

    add 5-1 error propagation

---
+++ Concurrency-in-Go/section5/1_error_propagation/propagation/myerror.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"runtime/debug"
+)
+
+// MyError is
+type MyError struct {
+	Inner      MyError
+	Message    string
+	StackTrace string
+	Misc       map[string]interface{}
+}
+
+func wrapError(err error, messagef string, msgArgs ...interface{}) MyError {
+	return MyError{
+		Inner:      err,
+		Message:    fmt.Sprintf(messagef, msgArgs...),
+		StackTrace: string(debug.Stack()),
+		Misc:       make(map[string]interface{}),
+	}
+}
+
+func (err MyError) Error() string {
+	return err.Message
+}
